422df860081104227910ddea666d3625d76c13de
sicou2 <sicou2@gmail.com>
2019-11-21 20:05:25 -0600

    Added EXPONATION function to ex21

--- ex21.py
+++ ex21.py
@@ -14,20 +14,24 @@ def divide(a, b):
     print(f"DIVIDING {a} / {b}")
     return a / b 
 
+def exponent(a, b):
+    print(f"EXPONATING {a} ** {b}")
+    return a ** b 
 
 print("Let's do some math with just functions!")
 
 age = add(30, 5)
 height = subtract(78, 4)
-weight = multiply(90, 2)
-iq = divide(100, 2)
+weight = multiply(14, 2)
+iq = divide(20, -2)
+love = exponent(2, 2)
 
-print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}, Love: {love}")
 
 
 # A puzzle for the extra credit, type it in anyway.
 print("Here is a puzzle.")
 
-what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+what = exponent(love, add(age, subtract(height, multiply(weight, divide(iq, love)))))
 
 print("What becomes: ", what, "Can you do it by hand?")
