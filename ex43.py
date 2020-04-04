ed037ab6d9d656b127d4d5a361d1ca32a89a2a74
sicou2 <sicou2@gmail.com>
2020-04-04 18:03:26 -0500

    Continued with the review, also realized Zed's explanation of ex 41-43 is awful and almost useless.

---
+++ ex43.py
@@ -0,0 +1,58 @@
+# Rooms: 
+    # enter, exit, go to other rooms you have already gone to/ lockout
+        #entrance, antichamber, chest, slime, orc (alter), hallway, crypt, 
+        #exit (outside), death/end_game
+# Actions: fight, rest, move
+# Characters: hero, goblin, orc, bugbear, skeleton, 
+# Objects: helmet, sword, gems
+
+class Room(object):
+    
+    def enter(self) # this means room_antichamber.enter()
+        pass
+    
+    def exit(self) 
+        pass
+    
+
+
+class Room_Antichamber(Room):
+
+    def enter(self)
+        pass
+        
+class Room_Chest(Room):
+
+    def enter(self)
+        pass
+        
+class Room_Slime(Room):
+
+    def enter(self)
+        pass
+        
+class Room_Alter(Room):
+
+    def enter(self)
+        pass
+        
+class Room_Hallway(Room):
+
+    def enter(self)
+        pass
+        
+class Room_Crypt(Room):
+
+    def enter(self)
+        pass
+        
+class Room_Outside(Room):
+
+    def enter(self)
+        pass
+        
+class Room_End_Game(Room):
+
+    def enter(self)
+        pass
+        
