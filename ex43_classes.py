71f847f5c6656e1a42f96da5334f50f46549d415
sicou2 <sicou2@gmail.com>
2020-04-07 17:16:25 -0500

    Added to Review, up to ex36

--- ex43_classes.py
+++ ex43_classes.py
@@ -1,66 +1,82 @@
 class Scene(object):
-
+    print('1')
+    
     def enter(self):
+        print('')
         pass
-  
-
-  
+    
 class Death(Scene):
-    print('death')
+    print('2')
+    
     def enter(self):
+        print('')
         pass
 
 class CentralCorridor(Scene):
-    print('You are in the central_corridor')
-#    def enter(self):
-#        pass
+    print('3')
+
+    def enter(self):
+        print('')
+        pass
         
 class LaserWeaponArmory(Scene):
-    print('fun')
+    print('4')
+    
     def enter(self):
+        print('')
         pass
         
 class TheBridge(Scene):
-
+    print('5')
+    
     def enter(self):
+        print('')
         pass
         
 class EscapePod(Scene):
-
+    print('6')
+    
     def enter(self):
+        print('')
         pass
    
 # ---------------------------------------------------   
         
 class Engine(object):
+    print('7')
     
     def __init__(self, scene_map):
+        print('10')
         pass
         
     def play(self):
+        print('11')
         pass
-        #this needs 
-        #print('This is in play')
-        #a_map.opening_scene
 
 # --------------------------------------------------
 
 class Map(object):
-    
+    print('8')
+        
     def __init__(self, start_scene):
+        print('9')
         pass
         
     def next_scene(self, scene_name):
+        print('')
         pass
         
     def opening_scene(self):
-        print('This is the opening scene.')
+        print('')        
         pass
         
 # ----------------------------------------------------
 
-        
-a_map = Map('ded')
+print('First')
+a_map = Map('central_corridor')
+print('Second')
 a_game = Engine(a_map)
+print('Third')
 a_game.play()
+print('Fourth')
 
---
+++ nubbin.py
@@ -0,0 +1,7 @@
+class Thing(object):
+		def test(message):
+			#message = "Howdy dammit!"
+			print(message)
+			
+a = Thing()
+a.test("Howdy")
