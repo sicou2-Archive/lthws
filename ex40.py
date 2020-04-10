d46e3857e796ba0c409d31166b2d43d4a20b80fe
sicou2 <sicou2@gmail.com>
2020-04-10 11:53:46 -0500

    review finished 40, started to follow book example in 43

--- ex40.py
+++ ex40.py
@@ -7,10 +7,13 @@ class Song(object):
         for line in self.lyrics:
             print(line)
         
-happy_bday = Song(["Happy birthday to you",
-                   "I don't want to get sued",
-                   "So I'll stop right there"])
-                   
+#happy_bday = Song(["Happy birthday to you",
+#                   "I don't want to get sued",
+#                   "So I'll stop right there"])
+ 
+
+happy_bday = Song('')
+
 bulls_on_parade = Song(["They rally around the family",
                         "With pockets full of shells"])
                         
--- ex43_classes.py
+++ ex43_classes.py
@@ -4,19 +4,17 @@ from textwrap import dedent #Remove any common leading whitespace from
                             #every line in text. Allows for allign left
                             #when dealing with ''' '''
 
-print((text))
-
-
-
-
-
-
 class Scene(object):
 
-    
     def enter(self):
-
-        pass
+        ''' 
+        This seems to tell the user that the called scene is not built
+        and that it needs to be added to the code. I wonder why every 
+        sub-class needs enter. Maybe we will eventually find out. 
+        ''' 
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+        exit(1) # I wonder what he has exit(1) set up to do. 
     
 class Death(Scene):
 
@@ -57,31 +55,52 @@ class EscapePod(Scene):
         
 class Engine(object):
 
-    
     def __init__(self, scene_map):
-
-        pass
+        self.scene_map = scene_map
         
     def play(self):
-
-        pass
+        current_scene = self.scene_map.opening_scene()
+        print(current_scene)
+        last_scene = self.scene_map.next_scene('finished')
+        
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+            
+        # Be sure to print out the last scene
+        current_scene.enter() # This line is causing a problem at the 
+                              # point in which I stopped. 
+    #########STOPPED HERE###################
 
 # --------------------------------------------------
 
-class Map(object):
+class Map(object): 
+    # I kind of want to believe that class Map and Engine should be one
+    # class together. 
+
+
+    scenes = { 
+        # This is a dict of all of the scenes 
+        # I wonder if it would be better placed in the scene class? 
+        # Maybe not, I will probably want to have a think about it. 
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        #'finished': Finished(), # This needs to be created above
+        }
 
         
     def __init__(self, start_scene):
-
-        pass
+        self.start_scene = start_scene
         
     def next_scene(self, scene_name):
-
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
         
     def opening_scene(self):
-   
-        pass
+        return self.next_scene(self.start_scene)
         
 # ----------------------------------------------------
 
